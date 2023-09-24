fc0e735e0a2d966ca15d59bd28a88bf42e806b08
vakulenk0 <vakulenko.mityusha@mail.ru>
2023-09-24 23:41:36 +0300

    add lab#2

---
+++ main.py
@@ -0,0 +1,35 @@
+import math
+import numpy
+#1
+a = 1
+b = -1000
+c = 1
+#2 +
+# a = 6
+# b = 5
+# c = -4
+#3 +
+# a = 6*pow(10, 30)
+# b = 5*pow(10, 30)
+# c = -4*pow(10, 30)
+#4
+# a = pow(10, -30)
+# b = -pow(10, 30)
+# c = pow(10, 30)
+#5 +
+# a = 1.0000000
+# b = -4.0000000
+# c = 3.9999999
+
+
+d = b*b - 4*a*c
+x1 = (-b+math.sqrt(d))/(2*a)
+x2 = (-b-math.sqrt(d))/(2*a)
+
+x12 = (-b+numpy.sign(b)*math.sqrt(d))/(2*a)
+x22 = c/(x1*a)
+
+if (x2 > x22):
+    print("x1: " + str(x1) + "   x2: " + str(x2))
+else:
+    print("x1: " + str(x1) + " x2: " + str(x22))
